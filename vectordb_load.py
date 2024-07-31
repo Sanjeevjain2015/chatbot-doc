21721c2daf06e49af059f7564bd1bfaa050d40ce
Sanjeevjain2015 <sanjeev.jain2005@gmail.com>
2024-08-01 01:40:03 +0530

    Add files via upload

--- vectordb_load.py
+++ vectordb_load.py
@@ -19,7 +19,9 @@ import textwrap
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 from dotenv import load_dotenv
+import os
 load_dotenv()
+google_api_key = os.environ["GOOGLE_API_KEY"]
 
 import streamlit as st
 
